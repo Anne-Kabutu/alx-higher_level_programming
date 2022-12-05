afca5ac9eaa63f6d036b2ee2282876dc9a19003b
Anne-Kabutu <annekabutu@gmail.com>
2022-12-05 10:42:47 -0800

    correcctions

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 for number in range(0, 100):
-    if number == 99:
-        print("{}".format(number))
+    if number < 10:
+        string = f"0{number:d}"
     else:
-        print("{:02}".format(number), end=", ")
+        string = f"{number:d}"
+    if number == 99:
+        print("{}".format(string))
+    else:
+        print("{}, ".format(string), end="")
